8e454a1e1a7d2ba1c71e2201450a4ec7ba86c30c
Fabio Ribeiro <faabiosr@gmail.com>
2019-12-15 13:29:00 -0300

    Call the generator
    Now the main file will generate the golang code using the developed
    packages.

--- cmd/protoc-gen-nats/main.go
+++ cmd/protoc-gen-nats/main.go
@@ -3,7 +3,12 @@ package main
 import (
 	"flag"
 	"fmt"
+	"log"
 	"os"
+
+	"github.com/faabiosr/proto-nats/internal/cli"
+	"github.com/faabiosr/proto-nats/internal/gen"
+	"github.com/faabiosr/proto-nats/internal/template"
 )
 
 const version = "0.0.0"
@@ -15,5 +20,16 @@ func main() {
 	if *vFlag {
 		fmt.Println(version)
 		os.Exit(0)
+	}
+
+	cli := cli.New(
+		gen.New(
+			version,
+			template.New(),
+		),
+	)
+
+	if err := cli.Run(os.Stdin, os.Stdout); err != nil {
+		log.Fatal(err)
 	}
 }
